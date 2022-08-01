ea89e4db2999c313c278640c04d39a4a73ba507e
Lília Paula <liliaxtz2@gmail.com>
2022-07-31 21:43:54 -0300

    feat: footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { FooterContainer } from "./styles";
+
+const Footer = () => {
+  return <FooterContainer>Developed by 🍄 Lília Paula</FooterContainer>;
+};
+export default Footer;
